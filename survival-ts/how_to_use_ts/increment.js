d09f12eec460005acb4c0b3d3ff0c664ee27d3c7
elspgoco0915 <elspgoco0915@gmail.com>
2023-06-26 21:12:07 +0900

    ディレクトリ整理

---
+++ survival-ts/how_to_use_ts/increment.js
@@ -0,0 +1,6 @@
+function increment(num) {
+    return num + 1;
+}
+console.log(increment(999)); //1000
+// Argument of type 'string' is not assignable to parameter of type 'number'.
+// console.log(increment("999"));  //"9991"
---
+++ survival-ts/how_to_use_ts/increment.ts
@@ -0,0 +1,7 @@
+function increment(num: number) {
+    return num + 1;
+}
+console.log(increment(999));    //1000
+
+// Argument of type 'string' is not assignable to parameter of type 'number'.
+// console.log(increment("999"));  //"9991"
