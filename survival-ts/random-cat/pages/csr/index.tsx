7751be733f4a6ab5124c27216a4b2aeaa9a17f54
elspgoco0915 <elspgoco0915@gmail.com>
2023-07-02 08:01:40 +0900

    猫画像ジェネレータ完成

--- survival-ts/random-cat/pages/csr/index.tsx
+++ survival-ts/random-cat/pages/csr/index.tsx
@@ -1,17 +1,18 @@
-// SSR版
-import { GetServerSideProps, NextPage } from "next";
-import { useState } from "react";
-import styles from "./index.module.css"
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
-// GetServerSidePropsから渡されるpropsの型
-type Props = {
-    initialImageUrl: string;
-};
-
-const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
-    const [imageUrl, setImageUrl] = useState(initialImageUrl);
+const IndexPage: NextPage = () => {
+    // useStateを使って状態を定義
+    const [imageUrl, setImageUrl] = useState("");
     const [loading, setLoading] = useState(true);
 
+    // マウント時に画像を読み込む宣言
+    useEffect(() => {
+        fetchImage().then((newImage) => {
+            setImageUrl(newImage.url); // 画像urlの状態を更新
+            setLoading(false); // ローディング状態を更新
+        });
+    }, []);
     // ボタンをクリックしたときに画像を読み込む処理
     const handleClick = async () => {
         setLoading(true);
@@ -20,27 +21,14 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
         setLoading(false);
     }
     return (
-        <div className={styles.page}>
+        <div>
             <button onClick={handleClick}>他を見る</button>
             {/* // ローディング中でなければ、画像を表示する */}
-            <div className={styles.frame}>
-                {loading || <img src={imageUrl} />}
-            </div>
+            <div>{loading || <img src={imageUrl} />}</div>
         </div>
     );
 };
 export default IndexPage;
-
-// サーバサイドで実行する処理
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
-    const image = await fetchImage();
-    return {
-        props: {
-            initialImageUrl: image.url,
-        }
-    }
-}
-
 
 type Image = {
     url: string;
