344819083b33383ea1cbaf1e1a25edb5f54adbce
elspgoco0915 <elspgoco0915@gmail.com>
2023-07-06 20:00:41 +0900

    4-4 event, style

--- react-from-basics/my-app/src/App.jsx
+++ react-from-basics/my-app/src/App.jsx
@@ -1,9 +1,19 @@
 // コンポーネントファイルは.jsxにして区別
 export const App = () => {
+
+    const onClickButton = () => { alert(); };
+
+    const contentStyle = {
+        color: "blue",
+        fontSize: "20px",
+    };
+
     return (
         <>
-            <h1>こんにちは</h1>
+            {console.log('test.')}
+            <h1 style={contentStyle}>こんにちは</h1>
             <p>お元気ですよ</p>
+            <button onClick={onClickButton}>ボタン</button>
         </>
     )
 }
