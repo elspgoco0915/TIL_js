1eb5f5c150bf7b124e96861c749796f09540012b
elspgoco0915 <elspgoco0915@gmail.com>
2023-07-22 09:12:37 +0900

    A-3 コンポーネント化

--- react-from-basics/memo-app/src/components/App.tsx
+++ react-from-basics/memo-app/src/components/App.tsx
@@ -1,12 +1,15 @@
-import { ChangeEvent, useState, FC } from "react";
+import { ChangeEvent, useState, FC, useCallback } from "react";
 import styled from "styled-components";
+import { MemoList } from "./MemoList";
 
 export const App: FC = () => {
   const [text, setText] = useState<string>("");
   const [memos, setMemos] = useState<string[]>([]);
 
+  // テキストボックス入力時に入力内容をStateに設定
   const onChangeText = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value);
 
+  // 追加ボタン押下時
   const onClickAdd = () => {
     const newMemos = [...memos];
     newMemos.push(text);
@@ -14,31 +17,19 @@ export const App: FC = () => {
     setText("");
   };
 
-  const onClickDelete = (index: number) => {
+  // [削除]ボタン押下時(何番目が押されたか引数で受け取る)
+  const onClickDelete = useCallback((index: number) => {
     const newMemos = [...memos];
     newMemos.splice(index, 1);
     setMemos(newMemos);
-  };
+  }, [memos]);
 
   return (
     <div>
       <h1>簡単メモアプリ</h1>
       <input type="text" value={text} onChange={onChangeText} />
       <SButton onClick={onClickAdd}>追加</SButton>
-      <SContainer>
-        <p>メモ一覧</p>
-        <ul>
-          {memos.map((memo, index) => (
-            <li key={memo}>
-              <SMemoWrapper>
-                <p>{memo}</p>
-                <SButton onClick={() => onClickDelete(index)} >削除</SButton>
-              </SMemoWrapper>
-            </li>
-          ))}
-        </ul>
-      </SContainer>
-
+      <MemoList memos={memos} onClickDelete={onClickDelete} />
     </div>
   );
 };
@@ -46,14 +37,3 @@ export const App: FC = () => {
 const SButton = styled.button`
     margin-left: 16px;
   `;
-
-const SContainer = styled.div`
-    border: solid 1px #ccc;
-    padding: 16px;
-    margin: 8px;
-  `;
-
-const SMemoWrapper = styled.div`
-  display: flex;
-  align-items: center;
-`;
