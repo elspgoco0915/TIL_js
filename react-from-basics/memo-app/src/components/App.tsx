a006645c8ee75dfa764a9e0d11e7b3cb46cba9de
elspgoco0915 <elspgoco0915@gmail.com>
2023-07-22 09:32:35 +0900

    A-4 カスタムフック化

--- react-from-basics/memo-app/src/components/App.tsx
+++ react-from-basics/memo-app/src/components/App.tsx
@@ -1,28 +1,25 @@
 import { ChangeEvent, useState, FC, useCallback } from "react";
 import styled from "styled-components";
 import { MemoList } from "./MemoList";
+import { useMemoList } from "../hooks/useMemoList";
 
 export const App: FC = () => {
+  const { memos, addTodo, deleteTodo } = useMemoList();
   const [text, setText] = useState<string>("");
-  const [memos, setMemos] = useState<string[]>([]);
 
   // テキストボックス入力時に入力内容をStateに設定
   const onChangeText = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value);
 
   // 追加ボタン押下時
   const onClickAdd = () => {
-    const newMemos = [...memos];
-    newMemos.push(text);
-    setMemos(newMemos);
+    addTodo(text);
     setText("");
   };
 
   // [削除]ボタン押下時(何番目が押されたか引数で受け取る)
   const onClickDelete = useCallback((index: number) => {
-    const newMemos = [...memos];
-    newMemos.splice(index, 1);
-    setMemos(newMemos);
-  }, [memos]);
+    deleteTodo(index);
+  }, [deleteTodo]);
 
   return (
     <div>
