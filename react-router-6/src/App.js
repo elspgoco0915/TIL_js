bc39c7e868841f20b05c1262559696eade5b881b
elspgoco0915 <elspgoco0915@gmail.com>
2024-03-02 06:39:04 +0900

    NavLinkの使い方

--- react-router-6/src/App.js
+++ react-router-6/src/App.js
@@ -1,15 +1,38 @@
 import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import Home from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact';
 import NoMatch from './pages/nomatch';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link, NavLink } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React Router v6</h1>
+      <ul>
+        <li>
+          {/* styleでの当て方 */}
+          <NavLink 
+            style={({ isActive }) => (isActive ? { color: 'red' } : undefined)} 
+            to="/"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          {/* classNameでの当て方 */}
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : undefined)} 
+            to="/about"
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
