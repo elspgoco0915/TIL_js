e923df927c3a4ed267a71cfd2e1cdf079ecf0f1b
elspgoco0915 <elspgoco0915@gmail.com>
2024-03-03 07:20:13 +0900

    useSearchParams

--- react-router-6/src/pages/contact.jsx
+++ react-router-6/src/pages/contact.jsx
@@ -4,6 +4,36 @@ import { useSearchParams } from "react-router-dom";
 function Contact (props) {
   const [searchParams, setSearchParams] = useSearchParams();
   const paramsValue = searchParams.get('product_name') || '';
+  const products = [
+    {
+      id: 1,
+      product_name: 'iPhone',
+      price: 1000,
+    },
+    {
+      id: 2,
+      product_name: 'iPad',
+      price: 500,
+    },
+    {
+      id: 3,
+      product_name: 'iPod',
+      price: 40,
+    },
+    {
+      id: 4,
+      product_name: 'MacBook',
+      price: 2000,
+    },
+  ];
+
+  const searchProducts = () => {
+    return products.filter((product) => {
+      return product.product_name.includes(
+        searchParams.get('product_name') || ''
+      );
+    })
+  };
 
   const handleChange = (event) => {
     const product_name = event.target.value;
@@ -21,6 +51,13 @@ function Contact (props) {
     <div>
       <h2>{props.message}</h2>
       <input type="text" onChange={handleChange} value={paramsValue} />
+      <ul>
+        {searchProducts().map((product) => (
+          <li key={product.id}>
+            {product.product_name}/{product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
