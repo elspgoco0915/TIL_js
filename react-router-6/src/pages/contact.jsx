91125bce4a8a55620b49e71c130f6d1c4d5c9402
elspgoco0915 <elspgoco0915@gmail.com>
2024-03-03 07:13:04 +0900

    useSearchParams

--- react-router-6/src/pages/contact.jsx
+++ react-router-6/src/pages/contact.jsx
@@ -1,9 +1,28 @@
 import { useLocation } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 function Contact (props) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const paramsValue = searchParams.get('product_name') || '';
+
+  const handleChange = (event) => {
+    const product_name = event.target.value;
+    if (product_name) {
+      setSearchParams({ product_name: event.target.value });
+    } else {
+      setSearchParams({});
+    }
+  };
+
   const location = useLocation();
   console.log(location);
-  return <h2>{props.message}</h2>
+
+  return (
+    <div>
+      <h2>{props.message}</h2>
+      <input type="text" onChange={handleChange} value={paramsValue} />
+    </div>
+  );
 }
 
 export default Contact;
