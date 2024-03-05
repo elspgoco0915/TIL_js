7102daa42b9c2cbff80aef08da8632337fe031a1
elspgoco0915 <elspgoco0915@gmail.com>
2024-03-06 08:04:17 +0900

    react-router-dom初期設定

--- react/tutorial-app/src/App.js
+++ react/tutorial-app/src/App.js
@@ -1,10 +1,28 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+
 const App = () => {
   return (
     <>
       <h1>tutorial-app</h1>
       <p>react.dev</p>
       <a href="https://ja.react.dev/learn">https://ja.react.dev/learn</a>
+      <Router />
     </>
+  );
+}
+
+const Router = () => {
+  return (
+  <>
+    <h2>Router</h2>
+    <ul>
+      <li>
+        <NavLink to="/">
+          Home
+        </NavLink>
+      </li>
+    </ul>
+  </>
   );
 }
 
