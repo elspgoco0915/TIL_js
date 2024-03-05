96d767ce19858954c10889252d6d6f2392b29b99
elspgoco0915 <elspgoco0915@gmail.com>
2024-03-06 08:25:30 +0900

    https://ja.react.dev/learn#writing-markup-with-jsx

--- react/tutorial-app/src/pages/quickstart.jsx
+++ react/tutorial-app/src/pages/quickstart.jsx
@@ -1,9 +1,36 @@
 const QuickStart = () => {
   return (
-    <>
-    <h1>QuickStart</h1>
-    </>
-  )
+    <div>
+      <h1>QuickStart</h1>
+      <MyButton />
+      <Profile />
+    </div>
+  );
 }
+
+const MyButton = () => {
+  return (
+    <button>I'm a Button</button>
+  );
+}
+
+const Profile = () => {  
+  const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+  };
+  
+  return (
+    <>
+      <h1>{user.name}</h1>
+      {/* https://ja.react.dev/learn#writing-markup-with-jsx */}
+      <img src={user.imageUrl} alt="" />
+    </>
+  );
+}
+
+
+
 
 export default QuickStart;
