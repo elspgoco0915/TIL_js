21e8e57751457e5684ddb4b784fa22c8a00d3db3
elspgoco0915 <elspgoco0915@gmail.com>
2024-03-11 09:12:01 +0900

    初めてのコンポーネント

--- react/tutorial-app/src/pages/first-component.jsx
+++ react/tutorial-app/src/pages/first-component.jsx
@@ -1,8 +1,19 @@
-const FirstComponent = () => {
+const Profile = () => {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3Am.jpg"
+      alt="Katherine Johnson"
+    />
+  );
+}
 
+const FirstComponent = () => {
   return (
     <>
       <h1>Your-First-Component</h1>
+      <Profile />
+      <Profile />
+      <Profile />
     </>
   );
 }
