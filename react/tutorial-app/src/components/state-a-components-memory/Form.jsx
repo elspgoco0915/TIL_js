310e9237bb5604c56e4e1d0b0a858e233185d3c7
elspgoco0915 <elspgoco0915@gmail.com>
2024-03-30 07:57:12 +0900

    「state：コンポーネントのメモリ」のチャレンジ問題途中まで

---
+++ react/tutorial-app/src/components/state-a-components-memory/Form.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const Form = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+  }
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+  }
+
+  const handleReset = () => {
+    setFirstName('');
+    setLastName('');
+  }
+
+  return (
+    <form onSubmit={e => e.preventDefault()}>
+      <input
+        placeholder="First name"
+        value={firstName}
+        onChange={handleFirstNameChange}
+      />
+      <input
+        placeholder="Last name"
+        value={lastName}
+        onChange={handleLastNameChange}
+      />
+      <h1>Hi, {firstName} {lastName}</h1>
+      <button onClick={handleReset}>Reset</button>
+    </form>
+  );
+}
+
+export default Form;
--- react/tutorial-app/src/pages/state-a-components-memory/App.jsx
+++ react/tutorial-app/src/pages/state-a-components-memory/App.jsx
@@ -1,7 +1,19 @@
+import Form from '../../components/state-a-components-memory/Form';
+import Gallery from '../../components/state-a-components-memory/Gallery';
+
+
 const App = () => {
   return (
     <>
-    <h1>state-a-components-memory</h1>
+      <h1>state-a-components-memory</h1>
+      <h2>Gallery</h2>
+      <p>同じコンポーネントを２回レンダーしても、それぞれ独立したstateを持つ（ローカル）</p>
+      <hr />
+      <Gallery />
+      <hr />
+      <Gallery />
+      <h2>チャレンジ問題</h2>
+      <Form />
     </>
   );
 }
