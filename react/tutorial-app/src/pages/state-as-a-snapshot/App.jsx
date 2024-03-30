b2b6e78b5002061a1cacee112deb998a472c91c4
elspgoco0915 <elspgoco0915@gmail.com>
2024-03-31 07:32:05 +0900

    stateはスナップショットである

--- react/tutorial-app/src/pages/state-as-a-snapshot/App.jsx
+++ react/tutorial-app/src/pages/state-as-a-snapshot/App.jsx
@@ -1,9 +1,11 @@
+import TrafficLight from "../../components/state-as-a-snapshot/TrafficLight";
 
 const App = () => {
 
   return (
     <>
       <h1>state-as-a-snapshot</h1>
+      <TrafficLight />
     </>
   )
 
