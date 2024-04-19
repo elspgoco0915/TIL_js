308ebaa0f97b9d6131afcd13b3046d377bc111d8
elspgoco0915 <elspgoco0915@gmail.com>
2024-04-20 04:26:15 +0900

    追記

--- react/react.dev/ts-react/src/App.tsx
+++ react/react.dev/ts-react/src/App.tsx
@@ -4,6 +4,7 @@ import MyButton from './components/MyButton';
 const App = () => {
   return (
     <div className="App">
+      <h2>TypeScript で React コンポーネントを書く方法</h2>
       <MyButton />
     </div>
   );
